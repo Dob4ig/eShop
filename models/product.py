6231bebf577a4b1652decb297e92c922cd4a6894
Dob4ig <vadick.progamer@gmail.com>
2023-03-08 22:38:01 +0300

    minor changes at db

--- models/product.py
+++ models/product.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, TIMESTAMP
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, Text
 from datetime import datetime
 from models.models import metadata
 
@@ -7,7 +7,7 @@ product = Table(
     "product",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False),
-    Column("description", String, nullable=False),
+    Column("name", String(50), nullable=False),
+    Column("description", Text, nullable=False),
     Column("added_at", TIMESTAMP, nullable=False, default=datetime.utcnow)
 )
