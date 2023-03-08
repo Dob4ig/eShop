9f87d7e387acc190239c4038ad003d385cbb8c94
Dob4ig <vadick.progamer@gmail.com>
2023-03-08 22:17:31 +0300

    init alembic & fix product model (max_lenght problem)

--- models/product.py
+++ models/product.py
@@ -1,13 +1,13 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP
 from datetime import datetime
+from models.models import metadata
 
-metadata = MetaData()
 
 product = Table(
     "product",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False, max_length=50),
+    Column("name", String, nullable=False),
     Column("description", String, nullable=False),
     Column("added_at", TIMESTAMP, nullable=False, default=datetime.utcnow)
 )
