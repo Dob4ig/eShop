94ddd4dea8b98c6f9f5da9605c91bdda0beaf6f9
Dob4ig <vadick.progamer@gmail.com>
2023-03-09 22:11:16 +0300

    add price column && minor migrations fix

--- src/product/models.py
+++ src/product/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, Text
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, Text, FLOAT
 from datetime import datetime
 from sqlalchemy import MetaData
 
@@ -8,9 +8,10 @@ metadata = MetaData()
 product = Table(
     "product",
     metadata,
-    Column("id", Integer, primary_key=True),
+    Column("id", Integer, primary_key=True, autoincrement=True),
     Column("name", String(50), nullable=False),
     Column("description", Text, nullable=False),
+    Column("price", FLOAT, nullable=False),
     Column("added_at", TIMESTAMP, nullable=False, default=datetime.utcnow),
     Column("seller_id", Integer, nullable=False)
 )
--- src/product/schemas.py
+++ src/product/schemas.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 
 
 class ProductAdd(BaseModel):
-    id: Optional[int] = Field(ge=0)
     name: str = Field(max_length=50)
     description: str
     seller_id: int = Field(ge=0)
+    price: float = Field(ge=0)
