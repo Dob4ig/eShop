92f7e827dd939dab75a6f1041f3b3d4c245d81b8
Dob4ig <vadick.progamer@gmail.com>
2023-03-10 17:26:16 +0300

    add auth

--- migrations/env.py
+++ migrations/env.py
@@ -10,7 +10,7 @@ from alembic import context
 
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 from src.product.models import metadata as metadata_product
-
+from src.auth.models import metadata as metadata_auth
 
 sys.path.append(os.path.join(sys.path[0], 'src'))
 
@@ -33,7 +33,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = (metadata_product,)
+target_metadata = (metadata_product, metadata_auth)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/main.py
+++ src/main.py
@@ -2,11 +2,24 @@ from fastapi import FastAPI
 import uvicorn
 
 from product.router import router as product_router
-
-
+from auth.base_config import auth_backend, fastapi_users
+from auth.schemas import UserRead, UserCreate
 app = FastAPI()
 
 app.include_router(product_router, prefix="/product", tags=["product"])
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='127.0.0.1', reload=True)
