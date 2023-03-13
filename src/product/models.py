ffaf1453ecf7eec43997249eb6b8e7f708cbfa12
Dob4ig <vadick.progamer@gmail.com>
2023-03-13 19:09:10 +0300

    Change Table() to table class

--- src/product/models.py
+++ src/product/models.py
@@ -1,3 +1,4 @@
+from database import Base
 from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, Text,\
     FLOAT, ForeignKey
 from datetime import datetime
@@ -7,12 +8,22 @@ from auth.models import user as user_model
 metadata = MetaData()
 
 
-product = Table(
-    "product",
-    metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("name", String(50), nullable=False),
-    Column("description", Text, nullable=False),
-    Column("price", FLOAT, nullable=False),
-    Column("added_at", TIMESTAMP, nullable=False, default=datetime.utcnow),
-    Column("seller_id", Integer, ForeignKey(user_model.c.id), nullable=False))
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(Text, nullable=False)
+    price = Column(FLOAT, nullable=False)
+    added_at = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
+    seller_id = Column(Integer, ForeignKey(user_model.c.id))
+
+    @property
+    def serialize(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "added_at": self.added_at,
+            "seller_id": self.seller_id
+        }
