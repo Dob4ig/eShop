1c669b5a98a5a525732b3c1a49fa6820565461ee
Dob4ig <vadick.progamer@gmail.com>
2023-03-16 01:10:35 +0300

    path config for alembic

--- migrations/env.py
+++ migrations/env.py
@@ -1,16 +1,16 @@
-import os
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-from alembic import context
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.database import Base
-from src.auth.models import User
-from src.product.models import Product
-from src.verifySeller.models import VerifyQuee
-import sys
+import migrations.path
 
-sys.path.append(os.path.join(sys.path[0], 'src'))
+
+from src.verifySeller.models import VerifyQuee
+from src.product.models import Product
+from src.auth.models import User
+from src.database import Base
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from alembic import context
+from sqlalchemy import pool
+from sqlalchemy import engine_from_config
+from logging.config import fileConfig
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
