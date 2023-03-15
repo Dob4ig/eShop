235b480df0e4bef97c874a3b3dc055d23d413a52
Dob4ig <vadick.progamer@gmail.com>
2023-03-16 01:10:54 +0300

    to prev commit

--- migrations/env.py
+++ migrations/env.py
@@ -1,16 +1,16 @@
 import migrations.path
 
-
-from src.verifySeller.models import VerifyQuee
-from src.product.models import Product
-from src.auth.models import User
-from src.database import Base
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy import engine_from_config
 from logging.config import fileConfig
 
+from src.database import Base
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from alembic import context
+
+from src.verifySeller.models import VerifyQuee
+from src.product.models import Product
+from src.auth.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
